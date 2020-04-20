939598c38bfb96f177e1c6ea4be91e2e20b52b2f
Michael Hsieh <mdhsieh8@gmail.com>
2020-04-19 18:05:17 -0700

    feat: Finish TLIB.05-Exercise-DrawEmojiOverFaces.
    Draw emojis over faces matching expressions. The default if
    no expression is found is a smiling emoji.
    Removed some commented out code.

--- app/src/main/java/com/example/android/emojify/Emojifier.java
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -7,10 +7,7 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-/*import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.RectF;
-import android.graphics.drawable.Drawable;*/
+
 import android.util.Log;
 import android.util.SparseArray;
 import android.widget.Toast;
@@ -71,7 +68,8 @@ public class Emojifier {
         // Log the number of faces
         Log.d(TAG, "Number of faces detected: " + numFaces);
 
-        Bitmap mResultBitmap = picture;
+        // Initialize result bitmap to original picture
+        Bitmap resultBitmap = picture;
 
         // If there are no faces detected, show a Toast message
         if (numFaces == 0)
@@ -118,17 +116,19 @@ public class Emojifier {
                     default:
                         // default to smile drawable
                         emojiBitmap = BitmapFactory.decodeResource(res, R.drawable.smile);
-                        Log.d(TAG, "Could not pick an emoji. Using smile emoji.");
+                        Toast.makeText(context, R.string.no_emoji, Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, "No emoji expression found. Using smile emoji.");
                 }
 
-                mResultBitmap = addBitmapToFace(mResultBitmap, emojiBitmap, thisFace);
+                // Add the emojiBitmap to the proper position in the original image
+                resultBitmap = addBitmapToFace(resultBitmap, emojiBitmap, thisFace);
             }
         }
 
         // Release the detector object once it is no longer needed
         detector.release();
 
-        return mResultBitmap;
+        return resultBitmap;
     }
 
     /**
@@ -256,58 +256,4 @@ public class Emojifier {
 
         return resultBitmap;
     }
-
-    /*static Bitmap detectBoundingRectangles(Context context, Bitmap picture)
-    {
-        Paint myRectPaint = new Paint();
-        myRectPaint.setStrokeWidth(5);
-        myRectPaint.setColor(Color.RED);
-        myRectPaint.setStyle(Paint.Style.STROKE);
-
-        Bitmap tempBitmap = Bitmap.createBitmap(picture.getWidth(), picture.getHeight(), Bitmap.Config.RGB_565);
-        Canvas tempCanvas = new Canvas(tempBitmap);
-        tempCanvas.drawBitmap(picture, 0, 0, null);
-
-        // Create the face detector, disable tracking and enable classifications
-        FaceDetector detector = new FaceDetector.Builder(context)
-                .setTrackingEnabled(false)
-                .setLandmarkType(FaceDetector.ALL_CLASSIFICATIONS)
-                .build();
-
-        if (!detector.isOperational()) {
-            // The face detector is not operational.
-            Toast.makeText(context, R.string.detector_not_operational, Toast.LENGTH_SHORT).show();
-            Log.d(TAG, "The required native library to do face detection is not available");
-        }
-
-        // Build the frame
-        Frame frame = new Frame.Builder().setBitmap(picture).build();
-
-        // Detect the faces
-        SparseArray<Face> faces = detector.detect(frame);
-
-        int numFaces = faces.size();
-
-        // Log the number of faces
-        Log.d(TAG, "Number of faces detected: " + numFaces);
-
-        for (int i = 0; i < numFaces; i++)
-        {
-            Face thisFace = faces.valueAt(i);
-
-            float x1 = thisFace.getPosition().x;
-            float y1 = thisFace.getPosition().y;
-            float x2 = x1 + thisFace.getWidth();
-            float y2 = y1 + thisFace.getHeight();
-            Log.d(TAG, "x and y of bounding rectangle's top left corner: " + x1 + ", " + y1);
-            Log.d(TAG, "width and height of bounding rectangle: " + x2 + ", " + y2);
-            tempCanvas.drawRoundRect(new RectF(x1, y1, x2, y2), 2, 2, myRectPaint);
-        }
-
-        // Release the detector object once it is no longer needed
-        detector.release();
-
-        //myImageView.setImageDrawable(new BitmapDrawable(getResources(),tempBitmap));
-        return tempBitmap;
-    }*/
 }
